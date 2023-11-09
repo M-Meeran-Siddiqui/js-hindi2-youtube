0658c42ca1339b2a2b9be32270c2a26d0a328269
M-Meeran-Siddiqui <137807639+M-Meeran-Siddiqui@users.noreply.github.com>
2023-11-09 16:02:55 +0000

    added 03_conversionOperation.js to this repo.

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,32 +1,35 @@
 // let score = 33
 // let score = "33"
-let score = "33abc"
-// let score = null;
+// let score = "33abc"
+let score = null;
 // let score = undefined;
+// let score ;
 // let score = true;
 
 //By first type: 
 // console.log( typeof score);
+// console.log(score)
 // By second type: 
 // console.log(typeof (score));
 
 let  valueInNumber = Number(score);  //here we do typecasting.
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
 
 
 
 // "33" => 33
 // "33abc" =>  gives value NaN(not a number) but its type is a number .
+// undefined => gives value NaN(not a number) but its type is undefined if we can't typecast.
 // true => 1; false => 0  
 // ""  => false;
 // "Meeran" => true;
 
 // let isLoggedIn = 1;
 //let isLoggedIn = "";
-let isLoggedIn = "Meeran";
+// let isLoggedIn = "Meeran";
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-// console.log(booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber);
